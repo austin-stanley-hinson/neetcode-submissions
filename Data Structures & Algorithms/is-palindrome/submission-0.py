65b7c2f2279989a91f730a83aa6ae8fbc330c2e3
Austin Stanley Hinson <austinstanleyh@gmail.com>
2026-06-21 11:09:56 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #Edge Case: s is an empty string
+        if not s:
+            return True
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue 
+            
+            if not s[r].isalnum():
+                r -= 1 
+                continue 
+            
+            if s[l].lower() != s[r].lower():
+                return False 
+            
+            l += 1 
+            r -= 1 
+        
+        return True 
+
+        #Dry Run 
+        '''
+        "tab a cat"
+           l.  r
+        '''
